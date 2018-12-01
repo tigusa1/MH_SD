a4d29ff0c324f100bbbf2327342c659fafff0b96
Qi <wangqi12240205@163.com>
2018-11-30 21:23:16 -0500

    clean up and update the noataion

--- SimpleSuicide_SD_qi.m
+++ SimpleSuicide_SD_qi.m
@@ -1,84 +1,81 @@
 function SimpleSuicide_SD_qi(handles)
-global a0  ac ar aa b0 Ic Ia Ir c
+global a0 a1 a2 ac ar aa Ic Ia Ir b_suicide
 global N Tmax
 %----------------------------------------------------------------------------------------------
 % SD model for suicide
 %----------------------------------------------------------------------------------------------
-% a0 = internal growth rate (set to 1)
-% ar = recovery program ES
-% ac = community program ES
-% aa = awareness program ES
-% b0 = base rate of suicidal tendency from PD
-% pn = proportion of non-suicidal that have PD
-% pn0= baseline proportion
+% parameters
+% a1 = base rate of recovery program from suicidal
+% a2 = internal growth rate (set to 1)
+% ar = recovery program 
+% ac = community program 
+% aa = awareness program 
+% a0 = base rate of suicidal tendency from PD
+% b_suicide = rate of commit suicide from suicidal
+% pn0= initial proportional PD in non-suicidal stock
+
+
+% flow Rate constants
+% internal growth                                = a2;                 
+% community intervention                         = ac*Ic;              
+% Sn with PD to suicidal (by awareness)          = a0*(1 - aa*Ia);     
+% Ss to Sr with PD (by recovery)                 = a1*(1-ar*Ir);       
+% Ss to Sr without PD (by recovery)              = a1*((ar)*Ir);       
+% Sr no PD to Sn no PD                           = 1;                   
+% Sr PD to Sn PD                                 = 1;                   
+% sproportional of Ss to commit suicide          = b_suicide;           
 %----------------------------------------------------------------------------------------------
-% CHANGE IN PD
-% internal:  +a0*Sn*(pn0 - pn)
-% community: -ac*Ic*Sn*pn
-% recovery:  -ar*Ir*Sr
-%
-% STOCKS
-% non-suicidal: Sn
-% suicidal:     Ss = b*Sn*pn
-% recovery:     Sr = Ss
-%
-% FLOW RATE
-% suicidal:     b  = b0*(1 - aa*Ia)
-%
-% EQUILIBRIUM
-% a0*(pn0 - pn) = ac*Ic*pn + ar*Ir*(b*pn)
-%           pn  = a0*pn0/[a0 + ac*Ic + ar*Ir*b] = pn0 (if no I)
-%
-% pn*Sn = pn*(N - Ss - Sr) = pn*(N - 2*Ss) = pn*(N - 2*b*Sn*pn)
-%    Sn = N - 2*b*Sn*pn
-%    Sn = N / (1 + 2*b*pn)
+
 %----------------------------------------------------------------------------------------------
-a0 = 1;
+% parameters initializaion
+a0 = 0.5;
+a1 = 0.3; 
+a2 = 0.1;
 ar = 0.4;
 ac = 0.6;
 aa = 0.5;
-b0 = 0.1;
+
 
 Ir = 0.50;
 Ic = 0.51;
 Ia = 0.52;
-c = 0.00001;
+b_suicide = 0.00001;
 
 pn0 = 0.3;
 
-% number of time steps
 
-tspan = linspace(0, Tmax, N); % unit: month
-
+% get the value by changing sliders 
 if nargin
-    ar = get(handles.ar,'Value'); handles.ar_txt.String = sprintf('ar = %.2f',ar);
-    ac = get(handles.ac,'Value'); handles.ac_txt.String = sprintf('ac = %.2f',ac);
-    aa = get(handles.aa,'Value'); handles.aa_txt.String = sprintf('aa = %.2f',aa);
-    b0 = get(handles.b0,'Value'); handles.b0_txt.String = sprintf('b0 = %.2f',b0);
-    Ir = get(handles.Ir,'Value'); handles.Ir_txt.String = sprintf('Ir = %.2f',Ir);
-    Ic = get(handles.Ic,'Value'); handles.Ic_txt.String = sprintf('Ic = %.2f',Ic);
-    Ia = get(handles.Ia,'Value'); handles.Ia_txt.String = sprintf('Ia = %.2f',Ia);
-    pn0= get(handles.Pn,'Value'); handles.Pn_txt.String = sprintf('Pn = %.2f',pn0);
+    a0 = get(handles.a0,'Value'); handles.a0_txt.String = sprintf('a0 = %.2f (base rate of suicidal tendency from PD)',a0);
+    a1 = get(handles.a1,'Value'); handles.a1_txt.String = sprintf('a1 = %.2f (base rate of recovery program from suicidal)',a1);
+    a2 = get(handles.a2,'Value'); handles.a2_txt.String = sprintf('a2 = %.2f (internal growth rate)',a2);
+    ar = get(handles.ar,'Value'); handles.ar_txt.String = sprintf('ar = %.2f (recovery program coefficient)',ar);
+    ac = get(handles.ac,'Value'); handles.ac_txt.String = sprintf('ac = %.2f (community program coefficient)',ac);
+    aa = get(handles.aa,'Value'); handles.aa_txt.String = sprintf('aa = %.2f (awareness raising coefficient)',aa);
+    Ir = get(handles.Ir,'Value'); handles.Ir_txt.String = sprintf('Ir = %.2f (recovery program index)',Ir);
+    Ic = get(handles.Ic,'Value'); handles.Ic_txt.String = sprintf('Ic = %.2f (community program index)',Ic);
+    Ia = get(handles.Ia,'Value'); handles.Ia_txt.String = sprintf('Ia = %.2f (awareness raising index)',Ia);
+    pn0= get(handles.Pn,'Value'); handles.Pn_txt.String = sprintf('Pn0 = %.2f (initial proportion of PD)' ,pn0);
+    b_suicide = get(handles.b_suicide,'Value'); handles.b_suicide_txt.String = sprintf('b_suicide = %.2f (rate of commit suicide from suicidal)',b_suicide);
 end
-SnnPD = pn0;
-W0 = [1-pn0 pn0 0 0 0];
-Tmax = 12;                            % change yearly
-N =  600;
 
-[SnPD, SnnPD, Ss, SrPD, SrnPD] = rk_Suicide_SD( W0);
-Y = [SnPD;SnnPD;Ss;SrPD;SrnPD;Ss*c];
-% TLTS = {'people with no suicidal thought \n but psychosocial distress',...
-%         'people with no suicidal thought \n or psychosocial distress',...
-%         'people with suicidal thought \n',...
-%         'people in discovery program with \n psychosocial distress',...
-%         'people in discovery program with \n no psychosocial distress',...
-%         'people committed suicide \n'};
+SnnPD = pn0;
+W0    = [1-SnnPD SnnPD 0 0 0 0];       % initial condition: all people in non-suicidal stock
+
+Tmax  = 20;                            % maximum time
+N     = 600;
+tspan = linspace(0, Tmax, N);          % unit: day
+
+% run the ODE model
+[SnPD, SnnPD, Ss, SrPD, SrnPD, Sdeath] = rk_Suicide_SD( W0);
+% combine the stocks into one matrix, for plot
+Y = [SnPD;SnnPD;Ss;SrPD;SrnPD;Sdeath];
 TLTS = {'S_n with PD',...
     'S_n without PD',...
     'S_s',...
     'S_r with PD',...
     'S_r without PD',...
-    'S_{death}'};
+    'S_{death}'};            % titles for each subplots
 
 if nargin
     h_axes = {handles.axes1,handles.axes2,handles.axes3,...
@@ -94,23 +91,26 @@ for k=1:size(Y,1)
         subplot(2,3,k),
     end
     plot(tspan,Y(k,:),'LineWidth',2),title(sprintf(TLTS{k})),
+    % set different y limit in subplots
     if k <5
         ylim([0,1])
-        if k > 2
-            ylim([0,0.4])
+        if k > 2   
+            ylim([0,0.6])
         end
     end
     if k ==5
-        ylim([0,0.06])
+        if max(Y(k,:)) <= 0.1
+        ylim([0,0.1])
+        else
+        ylim([0,0.3]) 
+        end
     end
-    xlim([0,12])
-    
-    
+    xlim([0,Tmax])  
     xlabel('time (year)')
 end
 
 
-function [SnPD, SnnPD, Ss,SrPD, SrnPD] = rk_Suicide_SD(W)
+function [SnPD, SnnPD, Ss,SrPD, SrnPD, Sdeath] = rk_Suicide_SD(W)
 %----------------------------------------------------------------------------------------------
 % Runge-Kutta solver for the suicide ODEs
 % W = Sn_PD Sn_nPD Ss Sr_Pd Sr_nPd
@@ -120,21 +120,17 @@ global Tmax N                                            % Tmax = maximum time (
 n    = size(W,1);                                        % number of samples
 Tmin = 0;                                                % minimum time
 h    = (Tmax - Tmin) / N;                                % time increment
-SnPD   = zeros(n,N);
+SnPD    = zeros(n,N);
 SnnPD   = zeros(n,N); Ss = SnPD;
-SrPD    = SnnPD;   SrnPD = SnnPD;                        % initialize the vecotors
+SrPD    = SnnPD;   SrnPD = SnnPD;  Sdeath =  SnnPD;      % initialize the vecotors
 for i = 1:N                                              % for each time step
     t       = Tmin + h*i;
-    %     SnPD(:,i) = max(W(:,1), 0);                        % stock of non suicidal with PD
-    %     SnnPD(:,i)= max(W(:,2), 0);                        % stock of suicidal without PD
-    %     Ss(:,i)   = max(W(:,3), 0);                        % stock of suicidal
-    %     SrPD(:,i) = max(W(:,4), 0);                        % stock of recovery with PD
-    %     SrnPD(:,i)= max(W(:,5), 0);                        % stock of recovery without PD
-    SnPD(:,i) = W(:,1);                        % stock of non suicidal with PD
-    SnnPD(:,i)= W(:,2);                        % stock of suicidal without PD
-    Ss(:,i)   = W(:,3);                        % stock of suicidal
-    SrPD(:,i) = W(:,4);                        % stock of recovery with PD
-    SrnPD(:,i)= W(:,5);                        % stock of recovery without PD
+    SnPD(:,i)    = W(:,1);                        % stock of non suicidal with PD
+    SnnPD(:,i)   = W(:,2);                        % stock of suicidal without PD
+    Ss(:,i)      = W(:,3);                        % stock of suicidal
+    SrPD(:,i)    = W(:,4);                        % stock of recovery with PD
+    SrnPD(:,i)   = W(:,5);                        % stock of recovery without PD
+    Sdeath(:,i)  = W(:,6);                        % stock of death
     k1 = fdW(W);                                    % integrate one step using Runge-Kutta
     k2 = fdW(W + k1 * h/2);
     k3 = fdW( W + k2 * h/2);
@@ -147,29 +143,30 @@ function dW = fdW(W)
 %----------------------------------------------------------------------------------------------
 % COPEWELL ODE:
 %   dW/dt = fdW(t,W)
-%   W     = Sn_PD Sn_nPD Ss Sr_Pd Sr_nPd
+%   W     = Sn_PD Sn_nPD Ss Sr_Pd Sr_nPd Sdeath
 %----------------------------------------------------------------------------------------------
 
 %----------------------------------------------------------------------------------------------
-global a0  ac ar aa b0 Ic Ia Ir c          % parameters for stocks into AT
+global a0 a1 a2 ac ar aa Ic Ia Ir b_suicide          % parameters for stocks into AT
 
 SnPD    = W(:,1);                        % stock of non suicidal with PD
 SnnPD   = W(:,2);                        % stock of suicidal without PD
 Ss      = W(:,3);                        % stock of suicidal
 SrPD    = W(:,4);                        % stock of recovery with PD
 SrnPD   = W(:,5);                        % stock of recovery without PD
+Sdeath  = W(:,6);                        % stock of death
 
 
 % rate constant
-nnPD_nPD_rate      = a0;                 
-nPD_nnPD_rate      = ac*Ic;
-nPD_s_rate         = b0*(1 - aa*Ia);
-s_rPD_rate         = a1*(1-ar*Ir);
-s_rnPD_rate        = a1*((ar)*Ir);
-rnPD_nnPD_rate     = 1;
-rPD_nPD_rate       = 1;
-s_suicide_rate     = c;
-
+nnPD_nPD_rate      = a2;                 % internal growth
+nPD_nnPD_rate      = ac*Ic;              % community intervention
+nPD_s_rate         = a0*(1 - aa*Ia);     % Sn with PD to suicidal (by awareness)
+s_rPD_rate         = a1*(1-ar*Ir);       % Ss to Sr with PD (by recovery)
+s_rnPD_rate        = a1*((ar)*Ir);       % Ss to Sr without PD (by recovery)
+rnPD_nnPD_rate     = 1;                  % Sr no PD to Sn no PD
+rPD_nPD_rate       = 1;                  % Sr PD to Sn PD
+s_suicide_rate     = b_suicide;          % proportional of Ss to commit suicide             
+death_birth_rate   = 1;
 % flow
 nnPD_nPD_flow      = nnPD_nPD_rate .* SnnPD;
 nPD_nnPD_flow      = nPD_nnPD_rate .* SnPD ;
@@ -179,17 +176,18 @@ s_rnPD_flow        = s_rnPD_rate   .* Ss;
 rnPD_nnPD_flow     = rnPD_nnPD_rate.* SrnPD;
 rPD_nPD_flow       = rPD_nPD_rate  .* SrPD;
 s_suicide_flow     = s_suicide_rate.* Ss;
-birth_flow         = s_suicide_flow;
+death_birth_flow   = death_birth_rate.* Sdeath;
 
 
 
 % Derivatives
 % temporary Derivatives
-dSnnPD = -nnPD_nPD_flow + nPD_nnPD_flow + rnPD_nnPD_flow + birth_flow;            % dSnnPD/dt
-dSnPD  = -nPD_nnPD_flow - nPD_s_flow + nnPD_nPD_flow +rPD_nPD_flow;
-dSs    = -s_rPD_flow - s_rnPD_flow - s_suicide_flow + nPD_s_flow;
-dSrPD  = - rPD_nPD_flow  + s_rPD_flow ;
+dSnnPD = -nnPD_nPD_flow + nPD_nnPD_flow + rnPD_nnPD_flow + death_birth_flow;            % dSnnPD/dt
+dSnPD  = -nPD_nnPD_flow - nPD_s_flow    + nnPD_nPD_flow  + rPD_nPD_flow;
+dSs    = -s_rPD_flow    - s_rnPD_flow   - s_suicide_flow + nPD_s_flow;
 dSrnPD = -rnPD_nnPD_flow + s_rnPD_flow;
+dSrPD  = - rPD_nPD_flow  + s_rPD_flow ;
+ddeath =  - death_birth_flow + s_suicide_flow ;
 
 % check if the temporary derivatives are feasible (net flow out is less than the stock). 
 % if not, than there is no outflow from the stock 
@@ -212,12 +210,17 @@ if SrnPD + dSrnPD <= 0
     rnPD_nnPD_flow = 0;
 end
 
-% real Derivatives 
-dSnnPD = -nnPD_nPD_flow + nPD_nnPD_flow + rnPD_nnPD_flow + birth_flow;            % dSnnPD/dt
-dSnPD  = -nPD_nnPD_flow - nPD_s_flow + nnPD_nPD_flow +rPD_nPD_flow;
-dSs    = -s_rPD_flow - s_rnPD_flow - s_suicide_flow + nPD_s_flow;
-dSrPD  = - rPD_nPD_flow  + s_rPD_flow ;
+if Sdeath + ddeath <= 0
+    death_birth_flow = 0;
+end
+
+% recalculate Derivatives 
+dSnnPD = -nnPD_nPD_flow + nPD_nnPD_flow + rnPD_nnPD_flow + death_birth_flow;            % dSnnPD/dt
+dSnPD  = -nPD_nnPD_flow - nPD_s_flow    + nnPD_nPD_flow  + rPD_nPD_flow;
+dSs    = -s_rPD_flow    - s_rnPD_flow   - s_suicide_flow + nPD_s_flow;
 dSrnPD = -rnPD_nnPD_flow + s_rnPD_flow;
+dSrPD  = - rPD_nPD_flow  + s_rPD_flow ;
+ddeath =  - death_birth_flow + s_suicide_flow ;
 
 
 dW(:,1) = dSnPD;
@@ -225,3 +228,4 @@ dW(:,2) = dSnnPD;
 dW(:,3) = dSs;
 dW(:,4) = dSrPD;
 dW(:,5) = dSrnPD;
+dW(:,6) = ddeath;
